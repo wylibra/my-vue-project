23ce0c11566118d849ac2b328f9be27088bf8b9a
wy <wangyuan@jingdata.com>
2018-07-10 12:19:23 +0800

    style: 项目样式目录结构调整

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import store from '@/store'
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import './views/styles/index.less'
+import './styles/index.less'
 import * as myFilters from '@/filters'
 
 // 全局注入 filters
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" class="main-container">
+  <div id="app" class="normal">
     <!--  router-view 当做是一个容器，它渲染的组件是你使用 vue-router 指定的 -->
     <router-view/>
   </div>
